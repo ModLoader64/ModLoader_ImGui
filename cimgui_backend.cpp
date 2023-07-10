fa54ad268a5482c7e2c4439e7cd8b582cb4351d3
Drahsid <Drahsid@gmail.com>
2023-07-09 20:26:50 -0400

    forward declare

--- cimgui_backend.cpp
+++ cimgui_backend.cpp
@@ -1,6 +1,33 @@
 #include "cimgui_backend.h"
-#include "imgui_impl_opengl3.h"
-#include "imgui_impl_sdl2.h"
+
+// forward declarations
+
+//// imgui_impl_opengl3
+// Backend API
+bool     ImGui_ImplOpenGL3_Init(const char* glsl_version);
+void     ImGui_ImplOpenGL3_Shutdown();
+void     ImGui_ImplOpenGL3_NewFrame();
+void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
+
+// (Optional) Called by Init/NewFrame/Shutdown
+bool     ImGui_ImplOpenGL3_CreateFontsTexture();
+void     ImGui_ImplOpenGL3_DestroyFontsTexture();
+bool     ImGui_ImplOpenGL3_CreateDeviceObjects();
+void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
+
+
+//// imgui_impl_sld2
+bool     ImGui_ImplSDL2_InitForOpenGL(SDL_Window* window, void* sdl_gl_context);
+bool     ImGui_ImplSDL2_InitForVulkan(SDL_Window* window);
+bool     ImGui_ImplSDL2_InitForD3D(SDL_Window* window);
+bool     ImGui_ImplSDL2_InitForMetal(SDL_Window* window);
+bool     ImGui_ImplSDL2_InitForSDLRenderer(SDL_Window* window, SDL_Renderer* renderer);
+void     ImGui_ImplSDL2_Shutdown();
+void     ImGui_ImplSDL2_NewFrame();
+bool     ImGui_ImplSDL2_ProcessEvent(const SDL_Event* event);
+
+
+
 
 //// imgui_impl_opengl3
 // Backend API
