05e175cbc67c612d76c826fca050a2a13b4e3f44
Drahsid <Drahsid@gmail.com>
2023-07-09 20:18:27 -0400

    backend wrapper

---
+++ cimgui_backend.cpp
@@ -0,0 +1,77 @@
+#include "cimgui_backend.h"
+
+//// imgui_impl_opengl3
+// Backend API
+CIMGUI_API bool     ImGuiBackend_ImplOpenGL3_Init(const char* glsl_version) {
+    return ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_Shutdown() {
+    ImGui_ImplOpenGL3_Shutdown();
+}
+
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_NewFrame() {
+    ImGui_ImplOpenGL3_NewFrame();
+}
+
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data) {
+    ImGui_ImplOpenGL3_RenderDrawData(draw_data);
+}
+
+// (Optional) Called by Init/NewFrame/Shutdown
+CIMGUI_API bool     ImGuiBackend_ImplOpenGL3_CreateFontsTexture() {
+    return ImGui_ImplOpenGL3_CreateFontsTexture();
+}
+
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_DestroyFontsTexture() {
+    ImGui_ImplOpenGL3_DestroyFontsTexture();
+}
+
+CIMGUI_API bool     ImGuiBackend_ImplOpenGL3_CreateDeviceObjects() {
+    return ImGui_ImplOpenGL3_CreateDeviceObjects();
+}
+
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_DestroyDeviceObjects() {
+    ImGui_ImplOpenGL3_DestroyDeviceObjects();
+}
+
+
+
+//// imgui_impl_sld2
+struct SDL_Window;
+struct SDL_Renderer;
+typedef union SDL_Event SDL_Event;
+
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForOpenGL(SDL_Window* window, void* sdl_gl_context) {
+    return ImGui_ImplSDL2_InitForOpenGL(window, sdl_gl_context);
+}
+
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForVulkan(SDL_Window* window) {
+    return ImGui_ImplSDL2_InitForVulkan(window);
+}
+
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForD3D(SDL_Window* window) {
+    return ImGui_ImplSDL2_InitForD3D(window);
+}
+
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForMetal(SDL_Window* window) {
+    return ImGui_ImplSDL2_InitForMetal(window);
+}
+
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForSDLRenderer(SDL_Window* window, SDL_Renderer* renderer) {
+    return ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+}
+
+CIMGUI_API void     ImGuiBackend_ImplSDL2_Shutdown() {
+    ImGui_ImplSDL2_Shutdown();
+}
+
+CIMGUI_API void     ImGuiBackend_ImplSDL2_NewFrame() {
+    ImGui_ImplSDL2_NewFrame();
+}
+
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_ProcessEvent(const SDL_Event* event) {
+    ImGui_ImplSDL2_ProcessEvent(event);
+}
+
+
--- cimgui_backend.h
+++ cimgui_backend.h
@@ -1,35 +1,35 @@
 #ifndef CIMGUI_BACKEND_H
 #define CIMGUI_BACKEND_H
 
-#include "cimgui.h" // IMGUI_IMPL_API
+#include "cimgui.h" // CIMGUI_API
 
 //// imgui_impl_opengl3
 // Backend API
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_Init(const char* glsl_version);
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
+CIMGUI_API bool     ImGuiBackend_ImplOpenGL3_Init(const char* glsl_version);
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_Shutdown();
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_NewFrame();
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
 
 // (Optional) Called by Init/NewFrame/Shutdown
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateFontsTexture();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyFontsTexture();
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateDeviceObjects();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
+CIMGUI_API bool     ImGuiBackend_ImplOpenGL3_CreateFontsTexture();
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_DestroyFontsTexture();
+CIMGUI_API bool     ImGuiBackend_ImplOpenGL3_CreateDeviceObjects();
+CIMGUI_API void     ImGuiBackend_ImplOpenGL3_DestroyDeviceObjects();
 
 
-/// imgui_impl_sld2
+//// imgui_impl_sld2
 struct SDL_Window;
 struct SDL_Renderer;
 typedef union SDL_Event SDL_Event;
 
-IMGUI_IMPL_API bool     ImGui_ImplSDL2_InitForOpenGL(SDL_Window* window, void* sdl_gl_context);
-IMGUI_IMPL_API bool     ImGui_ImplSDL2_InitForVulkan(SDL_Window* window);
-IMGUI_IMPL_API bool     ImGui_ImplSDL2_InitForD3D(SDL_Window* window);
-IMGUI_IMPL_API bool     ImGui_ImplSDL2_InitForMetal(SDL_Window* window);
-IMGUI_IMPL_API bool     ImGui_ImplSDL2_InitForSDLRenderer(SDL_Window* window, SDL_Renderer* renderer);
-IMGUI_IMPL_API void     ImGui_ImplSDL2_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplSDL2_NewFrame();
-IMGUI_IMPL_API bool     ImGui_ImplSDL2_ProcessEvent(const SDL_Event* event);
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForOpenGL(SDL_Window* window, void* sdl_gl_context);
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForVulkan(SDL_Window* window);
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForD3D(SDL_Window* window);
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForMetal(SDL_Window* window);
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_InitForSDLRenderer(SDL_Window* window, SDL_Renderer* renderer);
+CIMGUI_API void     ImGuiBackend_ImplSDL2_Shutdown();
+CIMGUI_API void     ImGuiBackend_ImplSDL2_NewFrame();
+CIMGUI_API bool     ImGuiBackend_ImplSDL2_ProcessEvent(const SDL_Event* event);
 
 #endif
 
